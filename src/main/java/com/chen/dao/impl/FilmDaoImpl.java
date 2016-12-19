07f48c4c0b96b88662cb3a23f2663aa6b1e4fde9
notsaltedfish <c1097210504@qq.com>
2016-12-19 20:09:08 +0800

    修改注释

--- src/main/java/com/chen/dao/impl/FilmDaoImpl.java
+++ src/main/java/com/chen/dao/impl/FilmDaoImpl.java
@@ -14,6 +14,7 @@ import com.chen.util.DbUtil;
 public class FilmDaoImpl implements FilmDao{
 
 	@Override
+	//根据条件查询电影
 	public List<Film> getFilms(String conditions) {
 		List<Film> films = new ArrayList<Film>();
 		Film film = null;
